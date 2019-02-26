81f4ac99d074e62627b908eb8f2408f1815f7803
WeiLun <we379875@gmail.com>
2019-02-26 08:19:52 +0800

    product desktop

--- shopping cart/js/all.js
+++ shopping cart/js/all.js
@@ -15,4 +15,28 @@ $(function () {
         " - $" + $("#slider-range").slider("values", 1));
 });
 
-
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        merge:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false,
+                mergeFit:true
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false,
+                mergeFit:false
+            }
+        }
+    })
+});
